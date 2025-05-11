a76c78358f79981c7a046de60f9c0fbc22a3e88b
Corentin Gobbo <corentin.gobbo@gmail.com>
2025-05-11 21:11:26 +0200

    update database connection settings to use Supabase

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,16 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        host: env("DATABASE_HOST", "db.vimehvwrkselgisjtigd.supabase.co"),
-        port: env("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "postgres"),
-        user: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "MicrosillonsTransferMdp31!"),
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env.bool('DATABASE_SSL', true) // Si DATABASE_SSL=true sur Render, active l'objet SSL
+        ? { rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false) }
+        : false,
     },
-    debug: false,
+    debug: env.bool("DATABASE_DEBUG", false),
   },
 });
